503efe1632f9270e4241f0570b528dda06824706
Alexey Kazakov <a.kazakov@betaonline.ru>
2022-07-20 16:02:16 +0300

    Homework-13 fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-// const authorization = require('./middlewares/authorization');
+const authorization = require('./middlewares/authorization');
 const pageNotFound = require('./middlewares/pageNotFound');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
@@ -14,14 +14,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(authorization);
-app.use((req, res, next) => {
-  req.user = {
-    _id: '5d8b8592978f8bd833ca8133',
-  };
-
-  next();
-});
+app.use(authorization);
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
 app.use(pageNotFound);
