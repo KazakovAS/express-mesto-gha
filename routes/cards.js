bd7093207c4bbf27cb7f81014a5f81287735a46c
Alexey Kazakov <a.kazakov@betaonline.ru>
2022-08-08 20:45:51 +0300

    homework-14 some

--- routes/cards.js
+++ routes/cards.js
@@ -1,3 +1,4 @@
+const { celebrate, Joi } = require('celebrate');
 const router = require('express').Router();
 const {
   getCards,
@@ -8,7 +9,12 @@ const {
 } = require('../controllers/cards');
 
 router.get('/', getCards);
-router.post('/', createCard);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    link: Joi.string().dataUri(),
+  }),
+}), createCard);
 router.delete('/:cardId', deleteCard);
 router.put('/:cardId/likes', setLikeCard);
 router.delete('/:cardId/likes', deleteLikeCard);
--- routes/createUser.js
+++ routes/createUser.js
@@ -1,8 +1,15 @@
+const { celebrate, Joi } = require('celebrate');
 const router = require('express').Router();
-const {
-  createUser,
-} = require('../controllers/users');
+const { createUser } = require('../controllers/users');
 
-router.post('/', createUser);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().dataUri(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
 
 module.exports = router;
